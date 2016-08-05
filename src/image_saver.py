75d400f974086b3c141af85f3451b35def6badb8
nmoucht <n.mouchtaris@aol.com>
2016-08-05 23:54:05 +0000

    Update image_saver.py

--- src/image_saver.py
+++ src/image_saver.py
@@ -1,16 +1,30 @@
-import cv2
-import cv_bridge
-from PIL import Image 
+'''
+This program gets the raw image from the AR Drone 2.0, and in a loop, publishes 
+the image, waits for the message, converts the raw image using Open CV, and saves 
+the image as droneIm(x).jpg. Though this code is in a seperate class, it can easily 
+be implemented in other python scripts.
+'''
+import cv2s
+from PIL import Image
 import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import Image as imageX
 from cv_bridge import CvBridge, CvBridgeError
+class image_save():
+     def __init__(self):
+	    self.bridge = CvBridge()  
+	    for x in range(0,3):
+            	self.image_pub = rospy.Publisher("/ardrone/image_raw",imageX) 
+   	    	msg=rospy.wait_for_message("/ardrone/image_raw", imageX)
+   	    	img = self.bridge.imgmsg_to_cv2(msg, "bgr8")    
+   	    	cv2.imwrite("droneIm"+str(x)+".jpg", img)
 
-def savePic(self):
-    for x in range(3):	
-           self.image_pub = rospy.Publisher("ardrone/image",imageX)   
-	   msg=rospy.wait_for_message('ardrone/image', imageX)
-	   self.bridge = CvBridge()  
-	   img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-	   cv2.imwrite("Droneim"+x+".jpg", img)
-self.savePic()
+def main():
+      rospy.init_node('save_picture', anonymous = True)
+      image_save()
+      
+if __name__ == '__main__': 
+ 	try: 
+		main()
+	except rospy.ROSInterruptException: 
+       			pass
