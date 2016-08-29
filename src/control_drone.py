b019671de6d40483f4ccf6934af298b827aa8804
nmoucht <n.mouchtaris@aol.com>
2016-08-29 20:54:19 +0000

    Update control_drone.py

--- src/control_drone.py
+++ src/control_drone.py
@@ -1,37 +1,62 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 '''
 Simple program to controll AR Drone 2.0
 Needed:
 ROS
 https://github.com/AutonomyLab/ardrone_autonomy.git
-
 ROS command for video feed:
 rosrun image_view image_view image:=/ardrone/image_raw
 '''
-import rospy 
-from std_msgs.msg import String 
-from std_msgs.msg import Empty 
+import rospy
+from std_msgs.msg import String
+from std_msgs.msg import Empty
 from geometry_msgs.msg import Twist
+import time as timer
 
-def takeoff(): 
-   pubT = rospy.Publisher("ardrone/takeoff", Empty, queue_size=10 ) 
-   rospy.init_node('takeoff', anonymous=True) 
-   rate = rospy.Rate(10) # 10hz 
-   for x in range(0,5): 
-       pubT.publish(Empty()) 
-       rate.sleep()
-   rospy.sleep(5)
-   commandTimer = rospy.Timer(rospy.Duration(100/1000.0),SendCommandF)
-   rospy.sleep(2)
-   commandTimer = rospy.Timer(rospy.Duration(100/1000.0),ResetCommand)
-   rospy.sleep(2)
-   commandTimer = rospy.Timer(rospy.Duration(100/1000.0),SendCommandB)
-   rospy.sleep(2)
-   commandTimer = rospy.Timer(rospy.Duration(100/1000.0),ResetCommand)
-   rospy.sleep(2)
-   land()
+rospy.init_node('takeoff', anonymous=True)
+pubT = rospy.Publisher("ardrone/takeoff", Empty, queue_size=1)
+commandDrone= rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+pubL = rospy.Publisher("ardrone/land", Empty, queue_size=1)
+rate = rospy.Rate(10) # 10hz
 
-def SendCommandF(self,roll=0,pitch=1,yaw_velocity=0,z_velocity=0):
+def control():
+	takeoff()
+	rospy.sleep(5)
+	z=True
+	time=0
+	while(z==True):
+		angle=raw_input("Enter an angle between 90 and -90, or 'land': ")
+		if(angle=="land"):
+			z=False
+		else:
+			a=float(angle)
+			if(a>0):
+				time=((4.3739*(10**-5))*(a**2))+((7.8375*(10**-3))*a)
+			elif(a<0):
+				a*=-1
+				time=((5.0178*(10**-5))*(a**2))+((0.0101)*a)
+		SendCommandS()
+		timer.sleep(time)
+		SendCommandF()
+		timer.sleep(0.5)
+		ResetCommand()
+		time=0
+	rospy.sleep(2)
+	land()
+	print "done."
+#39-23
+def takeoff():
+	print "Taking off..."
+	for x in range(0,5):
+		pubT.publish(Empty())
+		rate.sleep()
+def land():
+	print "Landing..."
+	for x in range(0,5):
+		pubL.publish(Empty())
+		rate.sleep()
+
+def SendCommandF(self,roll=0,pitch=0.75,yaw_velocity=0,z_velocity=0):
 	commandDrone= rospy.Publisher('/cmd_vel', Twist)
 	command=Twist()
 	command.linear.x = pitch
@@ -39,25 +64,14 @@ def SendCommandF(self,roll=0,pitch=1,yaw_velocity=0,z_velocity=0):
 	command.linear.z = z_velocity
 	command.angular.z = yaw_velocity
 	commandDrone.publish(command)
-	print command
 	#rospy.sleep(10)
 	#command.linear.x=-50
 	#commandDrone.publish(command)
-	
 #pitch +forward -backward
 #yaw +counterclockwise -clockwise
-def SendCommandB(self,roll=0,pitch=-1,yaw_velocity=0,z_velocity=0):
-	commandDrone= rospy.Publisher('/cmd_vel', Twist)
-	command=Twist()
-	command.linear.x = pitch
-	command.linear.y = roll
-	command.linear.z = z_velocity
-	command.angular.z = yaw_velocity
-	commandDrone.publish(command)
-	print command
 
-def ResetCommand(self,roll=0,pitch=0,yaw_velocity=0,z_velocity=0):
-	commandDrone= rospy.Publisher('/cmd_vel', Twist)
+def SendCommandS(roll=0,pitch=0,yaw_velocity=-0.75,z_velocity=0):
+	#commandDrone= rospy.Publisher('/cmd_vel', Twist)
 	command=Twist()
 	command.linear.x = pitch
 	command.linear.y = roll
@@ -65,14 +79,17 @@ def ResetCommand(self,roll=0,pitch=0,yaw_velocity=0,z_velocity=0):
 	command.angular.z = yaw_velocity
 	commandDrone.publish(command)
 
-def land():
-	rate = rospy.Rate(10)
-	pubL = rospy.Publisher("ardrone/land", Empty, queue_size=10 )
-	for x in range(0,5): 
-       		pubL.publish(Empty()) 
-       		rate.sleep()
-if __name__ == '__main__': 
-   try: 
-       takeoff() 
-   except rospy.ROSInterruptException: 
-       pass
+def ResetCommand(roll=0,pitch=0,yaw_velocity=0,z_velocity=0):
+	#commandDrone= rospy.Publisher('/cmd_vel', Twist)
+	command=Twist()
+	command.linear.x = pitch
+	command.linear.y = roll
+	command.linear.z = z_velocity
+	command.angular.z = yaw_velocity
+	commandDrone.publish(command)
+
+if __name__ == '__main__':
+	try:
+		control()
+	except rospy.ROSInterruptException:
+		pass
